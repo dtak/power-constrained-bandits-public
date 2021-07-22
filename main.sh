17b8830edb84de6868a0578aad87de900cb0e1b1
Jiayu <yaojiayu0826@gmail.com>
2021-07-22 03:45:41 -0400

    main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
-python run_exp.py --S 1000\  
- --env scb \ 		     
- --alg fix_pi\  		     
- --wrapper clip \ 	       
- --experiment power 	     
+python run_exp.py --S 1000 `# number of total simulations`\
+--env ascb `# environments (options: scb, ascb, mobile, nonlinear)`\
+--alg fix_pi `# algorithms (options: fix_pi, acts, bose, linucb)`\
+--wrapper clip `# wrapper algorithms (options: none, clip, drop, flip)`\
+--experiment power `# experiment type (options: power,type_i_error)`\
+--effect_size large # estimated noise size (options: none, small, large)
+
